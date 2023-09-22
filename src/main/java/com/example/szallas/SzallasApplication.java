610c3b08929aec0f86699dd68a18919b9eeca186
Rózsa István <ristvan98@gmail.com>
2023-09-22 09:51:12 +0200

    Spring inicializálása, java csomagok

---
+++ src/main/java/com/example/szallas/SzallasApplication.java
@@ -0,0 +1,13 @@
+package com.example.szallas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzallasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzallasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/szallas/SzallasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szallas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzallasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
