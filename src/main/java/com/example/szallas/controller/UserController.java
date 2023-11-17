fb0dc298aeda8791f73f4f5a2a5b8e9b06482300
Horváth Krisztián <kriszcso4@gmail.com>
2023-11-17 14:20:22 +0100

    Jelszó és Email update Backendrészen

--- src/main/java/com/example/szallas/controller/UserController.java
+++ src/main/java/com/example/szallas/controller/UserController.java
@@ -5,18 +5,23 @@ import com.example.szallas.model.request.RegistrationRequest;
 import com.example.szallas.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/register")
     public String registerPage(Model model){
@@ -47,5 +52,35 @@ public class UserController {
     @GetMapping("/user/profile")
     public String profilePage(){
         return "profile";
+    }
+
+    @PostMapping("/user/updateEmail")
+    public String updateEmail(@RequestParam("newEmail") String newEmail) {
+        User currentUser = userService.getCurrentLoggedInUser();
+
+        currentUser.setEmail(newEmail);
+        userService.updateUser(currentUser);
+
+        return "redirect:/user/profile?success";
+    }
+
+    @PostMapping("/user/updatePassword")
+    public String updatePassword(@RequestParam("password") String newPassword,
+                                 @RequestParam("passwordre") String confirmNewPassword) {
+        try {
+            if (!newPassword.equals(confirmNewPassword)) {
+                return "redirect:/user/profile?error=passwordMismatch";
+            }
+
+            User currentUser = userService.getCurrentLoggedInUser();
+
+            currentUser.setPassword(passwordEncoder.encode(newPassword));
+            userService.updateUser(currentUser);
+
+            return "redirect:/user/profile?success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/user/profile?error=unexpectedError";
+        }
     }
 }
