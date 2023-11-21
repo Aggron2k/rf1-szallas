7df6581779f3e798da170c81b78de86c0ecfc0bd
Hajas Attila <hajaattila@gmail.com>
2023-11-21 11:10:21 +0100

    reservationform.html, ReservationRequest update

--- src/main/java/com/example/szallas/model/request/ReservationRequest.java
+++ src/main/java/com/example/szallas/model/request/ReservationRequest.java
@@ -1,14 +1,27 @@
 package com.example.szallas.model.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
 @Getter
+@Data
 @Setter
 public class ReservationRequest {
-    private String checkinDate;
-    private String checkoutDate;
-    private int accomodationId;
+    @NotEmpty(message = "Az érkezés dátuma kötelező.")
+    private String checkInDate;
+
+    @NotEmpty(message = "A távozás dátuma kötelező.")
+    private String checkOutDate;
+
+    @NotNull(message = "A főszám megadása kötelező.")
+    private Integer numberOfPerson;
+
+    @NotEmpty(message = "A szállás azonosítója kötelező.")
+    private String accommodationId;
+
+    @NotEmpty(message = "A vendég azonosítója kötelező.")
+    private String customerId;
 }
